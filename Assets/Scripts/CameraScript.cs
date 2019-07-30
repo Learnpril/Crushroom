731006af5aacbadbbd6cb315c9d13912a3a971a5
Learnpril <emailing.april@gmail.com>
2019-07-30 00:38:33 -0700

    First Check-in

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public bool followingTarget;
+
+    public float cameraOffset;
+    public float smoothForCamera;
+    public GameObject target;
+
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(followingTarget)
+        {
+            targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+            //moves target ahead of player
+            Debug.Log(target.transform.localScale.x);
+
+            if (target.transform.localScale.x > 0f)
+            {
+                targetPosition = new Vector3(targetPosition.x + cameraOffset, targetPosition.y, targetPosition.z);
+            }
+            else
+            {
+                targetPosition = new Vector3(targetPosition.x - cameraOffset, targetPosition.y, targetPosition.z);
+            }
+
+            //transform.position = targetPosition;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothForCamera * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Transform checkForGround;
+    public LayerMask whatIsGround;
+
+    public bool isOnGround;
+
+    public float checkForGroundRadius;
+    public float movementSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D myRigidBody;
+
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isOnGround = Physics2D.OverlapCircle(checkForGround.position, checkForGroundRadius, whatIsGround);
+
+        if(Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myRigidBody.velocity = new Vector3(movementSpeed, myRigidBody.velocity.y, 0f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidBody.velocity = new Vector3(-movementSpeed, myRigidBody.velocity.y, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector3(0f, myRigidBody.velocity.y, 0f);
+        }
+
+        if (Input.GetButtonDown("Jump") && isOnGround == true)
+        {
+            myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, jumpSpeed, 0f);
+        }
+    }
+}
