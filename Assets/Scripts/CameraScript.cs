a8cd089bf64ffc1268e716245c06974c677d50e4
Learnpril <emailing.april@gmail.com>
2019-07-31 01:49:54 -0700

    Modified camera movement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -15,28 +15,35 @@ public class CameraScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {// 8====D~~~~~~~~~~~SPLASH 0___0;
+        //<3
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // gather data.
+
+        // use data.
+
         if(followingTarget)
         {
             targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
 
             //moves target ahead of player
-            Debug.Log(target.transform.localScale.x);
+            //Debug.Log(target.transform.localScale.x);
+            // moving left/right
+            // i think this might work...
+            float left = Input.GetKey("a") ? -cameraOffset : 0;
+            float right = Input.GetKey("d") ? cameraOffset : 0;
 
-            if (target.transform.localScale.x > 0f)
-            {
-                targetPosition = new Vector3(targetPosition.x + cameraOffset, targetPosition.y, targetPosition.z);
-            }
-            else
-            {
-                targetPosition = new Vector3(targetPosition.x - cameraOffset, targetPosition.y, targetPosition.z);
-            }
+            targetPosition = new Vector3(targetPosition.x + left + right, targetPosition.y, targetPosition.z);
+            //if (target.transform.localScale.x > 0f)
+            //{
+            //}
+            //else
+            //{
+            //    targetPosition = new Vector3(targetPosition.x + , targetPosition.y, targetPosition.z);
+            //}
 
             //transform.position = targetPosition;
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothForCamera * Time.deltaTime);
